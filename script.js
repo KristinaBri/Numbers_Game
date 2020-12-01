26801547784f227c15d262952182f1e1b537f0f4
KristinaBri <59978364+KristinaBri@users.noreply.github.com>
2020-12-01 19:04:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // when there is no input
+  if (!guess) {
+    displayMessage('Please make a guess!👀');
+
+    // when the guess is correct
+  } else if (guess === secretNumber) {
+    displayMessage('👏 Number is correct!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high...' : 'Too low...');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// Again! button
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+// when the guess is too high. The code before refactoring.
+//  else if (guess > secretNumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = 'Too high...';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = 'You lost the game!';
+//     document.querySelector('.score').textContent = 0;
+//   }
+//   // when the guess is too low......
+// } else if (guess < secretNumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = 'Too low...';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = 'You lost the game!';
+//     document.querySelector('.score').textContent = 0;
+//   }
+// }
